c13af3ceadeaf0bc5a263952d8d82f7396e65bc4
shreyajagtap964 <shreyajagtap964@gmail.com>
2025-07-30 10:33:22 +0530

    Add files via upload

---
+++ sahayak/app/page.tsx
@@ -0,0 +1,30 @@
+import AccordionFeatures from "./components/accordion-features";
+import BeforeAfter from "./components/before-after";
+import { Container } from "./components/container";
+// import CTA from "./components/cta";
+import FAQ from "./components/faq";
+import Footer from "./components/footer";
+import { Header } from "./components/header";
+import Hero from "./components/hero";
+// import LogoClouds from "./components/logo-clouds";
+import Pricing from "./components/pricing";
+// import { renderSchemaTags } from "./lib/seo";
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+      <Hero />
+      {/* <LogoClouds /> */}
+      <BeforeAfter />
+      <AccordionFeatures />
+      <Pricing />
+      <FAQ />
+      {/* <CTA /> */}
+      <Footer />
+      {/* {renderSchemaTags()} */}
+    </Container>
+    // text-green-500
+    // text-[#fd8f4b]
+  );
+}
